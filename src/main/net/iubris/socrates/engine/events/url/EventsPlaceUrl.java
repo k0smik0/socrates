51a49c64b3ed4e0ac84e04cd0be4b372fe45f723
Massimiliano Leone <massimiliano.leone@iubris.net>
2012-06-26 21:32:52 +0200

    fix license headers

--- src/main/net/iubris/socrates/engine/events/url/EventsPlaceUrl.java
+++ src/main/net/iubris/socrates/engine/events/url/EventsPlaceUrl.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyleft 2012 Massimiliano Leone - massimiliano.leone@iubris.net .
  * 
- * DetailsPlaceUrl.java is part of 'Socrates'
+ * EventsPlaceUrl.java is part of 'Socrates'
  * 
  * 'Socrates' is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
